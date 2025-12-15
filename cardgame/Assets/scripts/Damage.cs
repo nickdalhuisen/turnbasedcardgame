86f89c8a3d2181771c658432cad65043d93afed2
nickdalhuisen <ndalhuisen@ziggo.nl>
2025-12-15 13:40:38 +0100

    begin

---
+++ cardgame/Assets/scripts/Damage.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ cardgame/Assets/scripts/Managers/TurnManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class TurnManager : MonoBehaviour
+{
+    enum CurrentPlayer{
+        enemy,
+        Player
+    }
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
